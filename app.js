25032324fc8db86d19f036451a21e67a48197723
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-14 17:49:15 -0700

    update city state from control events

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 /* Imports */
 
 /* Get DOM Elements */
+// DOM for displayCity
 const cityHeader = document.getElementById('city-header');
 const cityClimate = document.getElementById('climate-image');
 const cityArchitecture = document.getElementById('architecture-image');
-const cityAttractions = document.getElementById('attractions-list');
-// const nameInput = document.getElementById('name-input');
-// const climateSelect = document.getElementById('climate-select');
-// const architectureSelect = document.getElementById('architecture-select');
-//const attractionInput = document.getElementById('attraction-input');
+const attractionsList = document.getElementById('attractions-list');
+// DOM for control events
+const nameInput = document.getElementById('name-input');
+const climateSelect = document.getElementById('climate-select');
+const architectureSelect = document.getElementById('architecture-select');
+const attractionInput = document.getElementById('attraction-input');
+const addAttractionButton = document.getElementById('add-attraction-button');
 
 /* State */
 let city = {
     name: 'Paradise City',
-    climate: 'mediterranean',
-    architecture: 'modern',
+    climate: 'tropical',
+    architecture: 'baroque',
     attractions: [],
 };
 
 /* Events */
+nameInput.addEventListener('input', () => {
+    city.name = nameInput.value;
+    displayCity();
+});
+
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
+    displayCity();
+});
+
+architectureSelect.addEventListener('change', () => {
+    city.architecture = architectureSelect.value;
+    displayCity();
+});
 
 /* Display Functions */
 function displayCity() {
@@ -28,6 +45,15 @@ function displayCity() {
     cityArchitecture.src = 'assets/' + city.architecture + '.jpeg';
 }
 
+/* function displayAttractions() {
+    for (let attraction of city.attractions) {
+        const li = document.createElement('li');
+        li.textContent = attraction;
+        attractionsList.append(li);
+    }
+} */
+
 // (don't forget to call any display functions you want to run on page load!)
 
 displayCity();
+// displayAttractions();
